94b01c7400dfb09f8ac8eb07283310bbcb656222
doonam <doolife@ncits.co.kr>
2020-01-09 18:01:31 +0900

    [expand] reset & mouseleave add

--- src/expand/js/util/expand.js
+++ src/expand/js/util/expand.js
@@ -11,6 +11,7 @@ class Expand{
         this.$target = "";
         this.initNum = 0;
         this.isOpen = false;
+        this.strReset = true;
         this.widthArr = [];
         this.listhArr = [];
 
@@ -20,6 +21,7 @@ class Expand{
     _init(){
         this._settings();
         this._controls();
+        this._listReset();
     }
 
     _settings(){
@@ -35,6 +37,10 @@ class Expand{
             if(this.isOpen === true) return;
             this.actionOver();
         });
+        $(document).on("mouseleave", ()=>{
+            if(this.isOpen === true) return;
+            this._listReset();
+        });
         this.$list.on("click", ()=>{
             if(this.isOpen === true) return;
             this.actionClick();
@@ -44,11 +50,19 @@ class Expand{
         });
     }
 
+    _listReset(){
+        let resetNum = (this.strReset) ? 0 : 0.3 ;
+        TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[0]}]`)], resetNum, {x:0, y:0, width:"", height:"", scale:1, top:0, right:"50%", bottom:0, left:0});
+        TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[1]}]`)], resetNum, {x:0, y:0, width:"", height:"", scale:1, top:0, right:0, bottom:"50%", left:"50%"});
+        TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[2]}]`)], resetNum, {x:0, y:0, width:"", height:"", scale:1, top:"50%", right:0, bottom:0, left:"50%"});
+        this.strReset = false;
+    }
+
     actionClick(){
         this.isOpen = true;
-        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`)], 0.5, {x:0, y:0, width:"100%", height:"100%", top:0, right:0, bottom:0, left:0, zIndex:5});
-        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-bg]")], 0.4, {alpha:0});
-        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-layer]")], 0.5, {alpha:1});
+        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`)], 0.5, {x:0, y:0, width:"100%", height:"100%", top:0, right:0, bottom:0, left:0});
+        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-bg]")], 0.4, {autoAlpha:0});
+        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-layer]")], 0.5, {autoAlpha:1});
 
         if (this.$target === this.listhArr[0]) {
             TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[1]}]`)], 0.5, {x:$(window).width() / 2, right:0});
@@ -59,19 +73,18 @@ class Expand{
         }else if (this.$target === this.listhArr[2]) {
             TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[0]}]`)], 0.5, {x:-$(window).width() / 2, left:0});
             TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[1]}]`)], 0.5, {x:-$(window).width() / 2, y:-$(window).height() / 2, width:$(window).width(), top:0, right:0});
-        }
-
-        TweenMax.set([this.$list], {zIndex:0});
+        };
     }
 
     actionClose(){
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[0]}]`)], 0.5, {x:0, width: $(window).width() / 2, left:0});
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[1]}]`)], 0.5, {x:0, y:0, width:$(window).width() / 2, height:$(window).height() / 2, top:0, right:0, bottom:"50%", left:"50%"});
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[2]}]`)], 0.5, {x:0, y:0, width:$(window).width() / 2, height:$(window).height() / 2, top:"50%", right:0, bottom:0, left:"50%"});
-        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-bg]")], 0.4, {alpha:1, scale:1.3});
-        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-layer]")], 0.5, {alpha:0, onComplete:()=>{
+        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-bg]")], 0.4, {autoAlpha:1});
+        TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-layer]")], 0.5, {autoAlpha:0, onComplete:()=>{
                 this.isOpen = false;
-                TweenMax.set(this.$list, {x:0, y:0, width:"", height:"", top:"", right:"", bottom:"", left:""});
+                this.strReset = true;
+                this._listReset();
             }
         });
     }
