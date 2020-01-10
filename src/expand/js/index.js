5a5c3b346de66726c131f2ff3d776de0ab6b6836
doonam <doolife@ncits.co.kr>
2020-01-10 09:55:40 +0900

    [expand] callback add

--- src/expand/js/index.js
+++ src/expand/js/index.js
@@ -3,5 +3,15 @@ import Expand from './util/expand';
 
 const expand = new Expand({
     el:"#contents",
-    interval:100
+    interval:100,
+    openCallback(){
+        console.log(`%c open | ${this.$target}`, "color:pink; font-size:15px;");
+    },
+    closeCallback(){
+        console.log(`%c close | ${this.$target}`, "color:pink; font-size:15px;");
+    }
+});
+
+expand.on("end",()=>{
+    console.log(`%c end | ${expand.$endTarget}`, "color:pink; font-size:15px;");
 });
--- src/expand/js/util/expand.js
+++ src/expand/js/util/expand.js
@@ -2,16 +2,23 @@ class Expand{
     constructor(opts){
         this.opts = $.extend(false ,{
             el:"#contents",
-            interval:100
+            interval:100,
+            openCallback(){
+
+            },
+            closeCallback(){
+
+            }
         }, opts);
 
         this.$wrap = $(this.opts.el).find("[data-expand='wrap']");
         this.$list = this.$wrap.find("[data-list]");
-        this.$close = $(".expand__close");
+        this.$close = this.$wrap.find("[data-close]");
         this.$target = "";
+        this.$endTarget = "";
         this.initNum = 0;
         this.isOpen = false;
-        this.strReset = true;
+        this.resetStr = true;
         this.widthArr = [];
         this.listhArr = [];
 
@@ -51,11 +58,11 @@ class Expand{
     }
 
     _listReset(){
-        let resetNum = (this.strReset) ? 0 : 0.3 ;
+        let resetNum = (this.resetStr) ? 0 : 0.3 ;
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[0]}]`)], resetNum, {x:0, y:0, width:"", height:"", scale:1, top:0, right:"50%", bottom:0, left:0});
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[1]}]`)], resetNum, {x:0, y:0, width:"", height:"", scale:1, top:0, right:0, bottom:"50%", left:"50%"});
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[2]}]`)], resetNum, {x:0, y:0, width:"", height:"", scale:1, top:"50%", right:0, bottom:0, left:"50%"});
-        this.strReset = false;
+        this.resetStr = false;
     }
 
     actionClick(){
@@ -74,19 +81,23 @@ class Expand{
             TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[0]}]`)], 0.5, {x:-$(window).width() / 2, left:0});
             TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[1]}]`)], 0.5, {x:-$(window).width() / 2, y:-$(window).height() / 2, width:$(window).width(), top:0, right:0});
         };
+        this.methodDepth("openCallback");
     }
 
     actionClose(){
+        this.$endTarget = this.$target;
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[0]}]`)], 0.5, {x:0, width: $(window).width() / 2, left:0});
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[1]}]`)], 0.5, {x:0, y:0, width:$(window).width() / 2, height:$(window).height() / 2, top:0, right:0, bottom:"50%", left:"50%"});
         TweenMax.to([this.$wrap.find(`[data-list=${this.listhArr[2]}]`)], 0.5, {x:0, y:0, width:$(window).width() / 2, height:$(window).height() / 2, top:"50%", right:0, bottom:0, left:"50%"});
         TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-bg]")], 0.4, {autoAlpha:1});
         TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-layer]")], 0.5, {autoAlpha:0, onComplete:()=>{
                 this.isOpen = false;
-                this.strReset = true;
+                this.resetStr = true;
+                this.$wrap.trigger("end");
                 this._listReset();
             }
         });
+        this.methodDepth("closeCallback");
     }
 
     actionOver(){
@@ -107,6 +118,14 @@ class Expand{
         TweenMax.to([this.$list.find("[data-bg]")], 0.5, {scale:1.3});
         TweenMax.to([this.$wrap.find(`[data-list=${this.$target}]`).find("[data-bg]")], 0.5, {scale:1});
     }
+
+    on(event, func){
+        this.$wrap.on(event, func);
+    };
+
+    methodDepth(funcValue){
+        if (typeof this.opts[`${funcValue}`] == "function") this.opts[`${funcValue}`].call(this);
+    };
 }
 
 export default Expand;
