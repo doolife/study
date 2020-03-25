e85dc250503729c3674d9610b7fadf5c8720dc3d
doonamlee <cyber0322@hanmail.net>
2020-03-25 10:55:18 +0900

    total number edit

--- src/movingslider/js/index.js
+++ src/movingslider/js/index.js
@@ -5,7 +5,7 @@ import Movingslider from './util/movingslider';
 let movingslider1 = new Movingslider({
     el:"#moving-slider1",
     idx:0,
-    total:5
+    total:4
 });
 
 // movingslider1.reset = 3;
