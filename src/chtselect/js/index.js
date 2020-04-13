5084af2f11c7989199d7c20ddeb5ca9ed3235a58
doonam <doolife@ncits.co.kr>
2020-04-13 14:39:56 +0900

    [chtselect] Delete and move to another storage

--- src/chtselect/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../sass/index.scss';
-import ChtSelect from './util/cht-select';
-
-const chtselect = new ChtSelect({
-    el:"#contents",
-    scale:1.1,
-    leftX:-50,
-    rightX:50
-});
